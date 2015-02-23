4e2115ecc60ecde838b2eed156f35c9a154f924a
Kevin Martensson <kevinmartensson@gmail.com>
2015-02-23 23:32:44 +0100

    Add support for sourcemaps
    Fixes #2.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var gutil = require('gulp-util');
 var through = require('through2');
 var regenerator = require('regenerator');
+var recast = require('recast');
+var applySourceMap = require('vinyl-sourcemaps-apply');
 
 module.exports = function (options) {
 	return through.obj(function (file, enc, cb) {
@@ -16,7 +18,26 @@ module.exports = function (options) {
 		}
 
 		try {
-			file.contents = new Buffer(regenerator.compile(file.contents.toString(), options).code);
+			var res;
+
+			if (file.sourceMap) {
+				var ast = regenerator.transform(recast.parse(file.contents.toString(), {
+					sourceFileName: file.relative
+				}), options);
+
+				res = recast.print(ast, {
+					sourceMapName: file.relative
+				});
+			} else {
+				res = regenerator.compile(file.contents.toString(), options);
+			}
+
+			file.contents = new Buffer(res.code);
+
+			if (res.map && file.sourceMap) {
+				applySourceMap(file, res.map);
+			}
+
 			this.push(file);
 		} catch (err) {
 			this.emit('error', new gutil.PluginError('gulp-regenerator', err, {fileName: file.path}));
--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 var assert = require('assert');
 var fs = require('fs');
 var gutil = require('gulp-util');
+var sourceMaps = require('gulp-sourcemaps');
 var regenerator = require('./');
 
 it('should transpile with Regenerator', function (cb) {
@@ -15,4 +16,31 @@ it('should transpile with Regenerator', function (cb) {
 	stream.write(new gutil.File({
 		contents: fs.readFileSync('fixture.js')
 	}));
+});
+
+it('should generate source maps', function (cb) {
+	var init = sourceMaps.init();
+	var write = sourceMaps.write();
+
+	init
+		.pipe(regenerator())
+		.pipe(write);
+
+	write.on('data', function (file) {
+		var contents = file.contents.toString();
+		assert.equal(file.sourceMap.sources[0], 'fixture.js');
+		assert(/regeneratorRuntime.mark/.test(contents));
+		assert(/sourceMappingURL=data:application\/json;base64/.test(contents));
+		cb();
+	});
+
+	init.write(new gutil.File({
+		cwd: __dirname,
+		base: __dirname,
+		path: 'fixture.js',
+		contents: fs.readFileSync('fixture.js'),
+		sourceMap: ''
+	}));
+
+	init.end();
 });
